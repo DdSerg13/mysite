0782f683a861ec3f3a9a6a39c0aed037e504ba6e
Your Name <you@example.com>
2021-07-14 18:03:22 +0500

    'comment'

--- fhtagn/views.py
+++ fhtagn/views.py
@@ -1,5 +1,7 @@
 # blog/views.py
-from django.views.generic import ListView, DetailView # новое 
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView # новое
+from django.urls import reverse_lazy # импортируем новые методы
  
 from .models import Post
  
@@ -9,6 +11,24 @@ class BlogListView(ListView):
     template_name = 'home.html'
  
  
-class BlogDetailView(DetailView): # новое
+class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+ 
+ 
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+ 
+ 
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+ 
+ 
+class BlogDeleteView(DeleteView): # Создание нового класса
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
